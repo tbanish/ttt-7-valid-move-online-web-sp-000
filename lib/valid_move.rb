5339122a1f17ada1b64839e7b052098d9cce37e6
Tom Banish <tombanish@gmail.com>
2019-11-17 17:19:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,8 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == false
     true
-  elsif index.between
+  elsif index.between?(0,8) != true
+    false
   else position_taken?(board, index) == true
     false
   end
