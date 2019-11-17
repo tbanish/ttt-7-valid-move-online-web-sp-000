8017f3b1e257b4ff694412c14b76bfaf575e7687
Tom Banish <tombanish@gmail.com>
2019-11-17 17:11:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken? == false
+  if position_taken?(board, index) == false
     true
-  else position_taken? == true
+  else position_taken?(board, index) == true
     false
 end
 
